8daf9f415e3510b8b6de163a5f761abc9cde25a5
Andy Mauragis <andrew@mauragis.com>
2020-11-25 20:32:31 -0500

    Refactor, adjust fire, add green fire

---
+++ README.md
@@ -0,0 +1,4 @@
+# Dragon Lamp
+Arduino code for the dragon lamp thing.
+
+Targets Feather M4 Express.
--- sketch_dragon_lamp.ino
+++ sketch_dragon_lamp.ino
@@ -1,198 +1,236 @@
-// targetting feather M4
+// Dragon Lamp code
+// targetting feather M4 express
 
 #include <Adafruit_NeoPixel.h>
 #include <Adafruit_NeoPixel_ZeroDMA.h>
 
-const int PIN_ON_BOARD_LED = 8;
-const int PIN_NEO_STRING = 12;
-const int PIN_MODE_BTN = 10;
-const int PIN_DIM_POT = 17;
+#define hue_deg_to_u16( D ) ( ( 65535L ) * D / 360 )
+#define pct_to_u8( P ) ( ( 255 ) * P / 100 )
 
-const int NEO_STRING_NUM_PIXELS = 21;
-const uint32_t ON_BOARD_LED_MAP = NEO_GRB;
+// pin assignments
+const int PIN_ON_BOARD_LED = 8;
+const int PIN_NEO_STRING   = 12;
+const int PIN_MODE_BTN     = 10;
+const int PIN_DIM_POT      = 17;
+
+// neopixel configs
+const int NEO_STRING_NUM_PIXELS   = 21;
+const uint32_t ON_BOARD_LED_MAP   = NEO_GRB;
 const uint32_t NEO_STRING_LED_MAP = NEO_GRBW;
 
+// debounce config
+const unsigned long debounce_delay = 50;
 
-Adafruit_NeoPixel_ZeroDMA on_board(1, PIN_ON_BOARD_LED, ON_BOARD_LED_MAP);
-Adafruit_NeoPixel_ZeroDMA leds(NEO_STRING_NUM_PIXELS, PIN_NEO_STRING, NEO_STRING_LED_MAP );
-
+// globals
+static Adafruit_NeoPixel_ZeroDMA on_board( 1, PIN_ON_BOARD_LED, ON_BOARD_LED_MAP );
+static Adafruit_NeoPixel_ZeroDMA leds( NEO_STRING_NUM_PIXELS, PIN_NEO_STRING, NEO_STRING_LED_MAP );
 static uint8_t brightness_level = 8;
 
-// debouncing state
-static int button_state = HIGH;
-static int last_button_state = HIGH;
-static unsigned long last_debounce_time = 0;
-static unsigned long debounce_delay = 50;
+// fwd declare lighting functions
+void lighting_white();
+void lighting_red();
+void lighting_rainbow();
+void lighting_fire();
+void lighting_green_fire();
 
-static uint32_t fire_last_ts = 0;
+// global array for holding lighting functions
+void ( *lighting_functions[] )() = { lighting_white, lighting_red, lighting_rainbow, lighting_fire, lighting_green_fire };
+const int num_lighting_modes     = sizeof( lighting_functions ) / sizeof( lighting_functions[ 0 ] );
+static int lighting_idx          = 3; // active lighting function index
 
-//typedef enum {
-//  WHITE = 0,
-//  FIRE = 1,
-//  GREEN_FIRE = 2 ,
-//  RAINBOW = 3,
-//} lighting_mode_t;
-
-//lighting_mode_t lighting_mode;
-
-uint8_t lighting_mode = 4;
-
-//static uint32_t rainbow_state;
-
-void setup() {
-
-  randomSeed(analogRead(A0));
-
-  on_board.begin();
-  on_board.setBrightness(64);
-  for (int i = 0; i < on_board.numPixels(); i++) {
-    on_board.setPixelColor(i, on_board.Color(255, 255, 255));
-  }
-  on_board.show();
-
-  leds.begin();
-  leds.setBrightness(brightness_level);
-
-  pinMode(PIN_MODE_BTN, INPUT);
-  analogReadResolution(8);
-
-}
-
-void check_lighting_mode()
+// ███████╗███████╗████████╗██╗   ██╗██████╗
+// ██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗
+// ███████╗█████╗     ██║   ██║   ██║██████╔╝
+// ╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝
+// ███████║███████╗   ██║   ╚██████╔╝██║
+// ╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝
+void setup()
 {
-  int mode_button_now = digitalRead(PIN_MODE_BTN);
+    randomSeed( analogRead( A0 ) ); // A0 floating
 
-  if (mode_button_now != last_button_state) {
-    last_debounce_time = millis();
-  }
-
-  if ((millis() - last_debounce_time) > debounce_delay) {
-    // whatever the reading is at, it's been there for longer than the debounce
-    // delay, so take it as the actual current state:
-
-    // if the button state has changed:
-    if (mode_button_now != button_state) {
-      button_state = mode_button_now;
-
-      if (button_state == LOW) {
-        //        lighting_mode = WHITE; //( lighting_mode + 1 ) % 4;
-        lighting_mode = (lighting_mode + 1) % 6;
-        Serial.print("New Lighting Mode: ");
-        Serial.print(lighting_mode, DEC);
-        Serial.print("\n");
-      }
+    on_board.begin();
+    on_board.setBrightness( 8 );
+    for( int i = 0; i < on_board.numPixels(); ++i )
+    {
+        on_board.setPixelColor( i, on_board.Color( 255, 255, 255 ) );
     }
-  }
-  last_button_state = mode_button_now;
+    on_board.show();
+
+    leds.begin();
+    leds.setBrightness( brightness_level );
+
+    pinMode( PIN_MODE_BTN, INPUT );
+    analogReadResolution( 8 );
 }
 
+// debounced check for a negative edge on the mode button, increment global lighting index
+void check_mode_btn()
+{
+    static int button_state                 = HIGH;
+    static int last_button_state            = HIGH;
+    static unsigned long last_debounce_time = 0;
+    int mode_button_now                     = digitalRead( PIN_MODE_BTN );
+
+    if( mode_button_now != last_button_state )
+        last_debounce_time = millis();
+
+    if( ( millis() - last_debounce_time ) > debounce_delay )
+    {
+        // if the button state has changed:
+        if( mode_button_now != button_state )
+        {
+            button_state = mode_button_now;
+
+            if( button_state == LOW )
+            {
+                lighting_idx = ( lighting_idx + 1 ) % num_lighting_modes;
+                Serial.print( "New Lighting Mode: " );
+                Serial.print( lighting_idx, DEC );
+                Serial.print( "\n" );
+            }
+        }
+    }
+    last_button_state = mode_button_now;
+}
+
+// check adc pin for brightness setting, globally apply
 void check_brightness()
 {
-  // read wiper
-  int wiper = analogRead(PIN_DIM_POT);
-  //  Serial.print("wiper: ");
-  //  Serial.print(wiper, DEC);
-  //  Serial.print("\n");
+    brightness_level = analogRead( PIN_DIM_POT );
 
-  brightness_level = wiper;
-  on_board.setBrightness(brightness_level);
-  on_board.show();
-
-  leds.setBrightness(brightness_level);
+    leds.setBrightness( brightness_level );
 }
 
-void rainbow_effect()
+void lighting_solid( uint8_t r, uint8_t g, uint8_t b, uint8_t w )
 {
-  uint32_t rainbow_start = micros() / 24;
-  for (int i = 0; i < leds.numPixels(); i++) {
-    uint32_t pixelHue = rainbow_start + (i * 65536L / leds.numPixels());
-    leds.setPixelColor(i, leds.gamma32(leds.ColorHSV(pixelHue)));
-  }
-}
-
-void fire_effect()
-{
-  const uint16_t hue_min = 0;
-  const uint16_t hue_max = (65535L) * 60 / 360;
-  const uint16_t hue_range = hue_max - hue_min;
-
-
-  uint32_t now = millis();
-
-  if ((now - fire_last_ts) > 100)
-  {
-    fire_last_ts = now;
-    Serial.print("Updated fire\n");
-    uint32_t seed = random(hue_min, hue_max);
-
-    for (int i = 0; i < leds.numPixels(); i++) {
-      uint32_t pixelHue = (seed + (i * (hue_range) / leds.numPixels())) % hue_range - hue_min;
-      leds.setPixelColor(i, leds.gamma32(leds.ColorHSV(pixelHue)));
+    for( int i = 0; i < leds.numPixels(); ++i )
+    {
+        leds.setPixelColor( i, leds.Color( r, g, b, w ) );
     }
-  }
-  
 }
 
-void loop() {
+void lighting_white()
+{
+    lighting_solid( 128, 128, 128, 255 );
+}
 
-  check_lighting_mode();
+void lighting_red()
+{
+    lighting_solid( 255, 0, 0, 0 );
+}
 
-  check_brightness();
+void lighting_rainbow()
+{
+    uint32_t rainbow_start = micros() / 24;
+    for( int i = 0; i < leds.numPixels(); ++i )
+    {
+        uint32_t pixelHue = rainbow_start + ( i * 65536L / leds.numPixels() );
+        leds.setPixelColor( i, leds.gamma32( leds.ColorHSV( pixelHue ) ) );
+    }
+}
 
+void lighting_green_fire()
+{
+    const uint32_t update_interval = 50; // ms
 
-  switch (lighting_mode) {
-    default:
-    case 0:
-      for (int i = 0; i < leds.numPixels(); i++) {
-        leds.setPixelColor(i, leds.Color(0, 0, 0, 255));
-      }
-      break;
+    const uint32_t base_color = leds.gamma32( leds.ColorHSV( hue_deg_to_u16( 110 ), pct_to_u8( 90 ), pct_to_u8( 100 ) ) );
 
-    case 1:
-      for (int i = 0; i < leds.numPixels(); i++) {
-        leds.setPixelColor(i, leds.Color(255, 0, 0, 0));
-      }
-      break;
+    const uint8_t num_rdm_pix = 12;
+    const uint16_t rdm_hue_lo = hue_deg_to_u16( 75 );
+    const uint16_t rdm_hue_hi = hue_deg_to_u16( 180 );
+    const uint8_t rdm_sat_lo  = pct_to_u8( 60 );
+    const uint8_t rdm_sat_hi  = pct_to_u8( 100 );
 
-    case 2:
-      for (int i = 0; i < leds.numPixels(); i++) {
-        leds.setPixelColor(i, leds.Color(0, 255, 0, 0));
-      }
-      break;
+    static uint32_t last_update = 0;
 
-    case 3:
-      for (int i = 0; i < leds.numPixels(); i++) {
-        leds.setPixelColor(i, leds.Color(0, 0, 255, 0));
-      }
-      break;
+    uint32_t now = millis();
 
-    case 4:
-      rainbow_effect();
-      break;
+    if( ( now - last_update ) <= update_interval )
+        return;
 
-    case 5:
-      fire_effect();
-      break;
+    last_update = now;
 
+    // apply base color
+    for( int i = 0; i < leds.numPixels(); ++i )
+    {
+        leds.setPixelColor( i, base_color );
+    }
 
-  }
+    for( int i = 0; i < num_rdm_pix; ++i )
+    {
+        if( i < num_rdm_pix / 3 ) // make a third of the pixels some degree of purple
+        {
+            int pix_ix        = random( 0, leds.numPixels() );
+            uint16_t hue      = hue_deg_to_u16( 280 );
+            uint8_t sat       = random( rdm_sat_lo, rdm_sat_hi );
+            const uint8_t val = pct_to_u8( 100 );
 
-  leds.show();
+            leds.setPixelColor( pix_ix, leds.gamma32( leds.ColorHSV( hue, sat, val ) ) );
+        }
+        else // the rest use the random parameters
+        {
+            int pix_ix        = random( 0, leds.numPixels() );
+            uint16_t hue      = random( rdm_hue_lo, rdm_hue_hi );
+            uint8_t sat       = random( rdm_sat_lo, rdm_sat_hi );
+            const uint8_t val = pct_to_u8( 100 );
 
+            leds.setPixelColor( pix_ix, leds.gamma32( leds.ColorHSV( hue, sat, val ) ) );
+        }
+    }
+}
 
-  //  uint16_t i;
-  //
-  //  // Rainbow cycle
-  //  uint32_t elapsed, t, startTime = micros();
-  //  for(;;) {
-  //    t       = micros();
-  //    elapsed = t - startTime;
-  //    if(elapsed > 5000000) break; // Run for 5 seconds
-  //    uint32_t firstPixelHue = elapsed / 32;
-  //    for(i=0; i<strip.numPixels(); i++) {
-  //      uint32_t pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
-  //      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
-  //    }
-  //    strip.show();
-  //  }
+void lighting_fire()
+{
+    const uint32_t update_interval = 50; // ms
+
+    const uint32_t base_color = leds.gamma32( leds.ColorHSV( hue_deg_to_u16( 30 ), pct_to_u8( 85 ), pct_to_u8( 100 ) ) );
+
+    const uint8_t num_rdm_pix = 5;
+    const uint16_t rdm_hue_lo = hue_deg_to_u16( 0 );
+    const uint16_t rdm_hue_hi = hue_deg_to_u16( 55 );
+    const uint8_t rdm_sat_lo  = pct_to_u8( 60 );
+    const uint8_t rdm_sat_hi  = pct_to_u8( 100 );
+
+    static uint32_t last_update = 0;
+
+    uint32_t now = millis();
+
+    if( ( now - last_update ) <= update_interval )
+        return;
+
+    last_update = now;
+
+    // apply base color
+    for( int i = 0; i < leds.numPixels(); ++i )
+    {
+        leds.setPixelColor( i, base_color );
+    }
+
+    for( int i = 0; i < num_rdm_pix; ++i )
+    {
+        int pix_ix        = random( 0, leds.numPixels() );
+        uint16_t hue      = random( rdm_hue_lo, rdm_hue_hi );
+        uint8_t sat       = random( rdm_sat_lo, rdm_sat_hi );
+        const uint8_t val = pct_to_u8( 100 );
+
+        leds.setPixelColor( pix_ix, leds.gamma32( leds.ColorHSV( hue, sat, val ) ) );
+    }
+}
+
+// ██╗      ██████╗  ██████╗ ██████╗
+// ██║     ██╔═══██╗██╔═══██╗██╔══██╗
+// ██║     ██║   ██║██║   ██║██████╔╝
+// ██║     ██║   ██║██║   ██║██╔═══╝
+// ███████╗╚██████╔╝╚██████╔╝██║
+// ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝
+void loop()
+{
+    check_mode_btn();
+    check_brightness();
+
+    // call lighting function from table
+    lighting_functions[ lighting_idx ]();
+
+    // paint leds
+    leds.show();
 }
